9c987e7a2be3c7cf7143267c81d338fc6f05d76b
mdsiamul <mdsiamulsuratprince@gmail.com>
2022-07-16 19:17:08 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  const addCount = () => { setCount(count => count + 1) };
+  const lessCount = () => { setCount(count => count - 1) }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Prince Counter app</h1>
+      <h4> Number: {count}</h4>
+      <button onClick={addCount}>Add</button>
+      <button onClick={lessCount}>Less</button>
     </div>
   );
 }
